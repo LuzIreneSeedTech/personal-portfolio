0d3fc426fda1bd394112efe0457b666746212332
LuzIreneSeedTech <139109623+LuzIreneSeedTech@users.noreply.github.com>
2023-09-06 11:13:17 +0800

    landing feature done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "./styles/style.css";
 import Header from "./organisms/Header";
+import Landing from "./organisms/Landing";
 
 function App() {
   return (
     <>
       <div className="landing">
         <Header />
+        <Landing />
       </div>
     </>
   );
